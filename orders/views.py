b82d98bf6c290f3d62744bc68ba2be23321b2098
xprilion <xprilion@gmail.com>
2019-12-05 19:33:13 +0530

    Init

---
+++ orders/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Order
+from .forms import OrderForm
+from django.http import JsonResponse
+
+def indexView(request):
+    orders = Order.objects.all()
+    orders = list(orders.values())
+    return JsonResponse({'orders': orders})
+
+def viewOrder(request, orderId):
+    order = Order.objects.filter(id=orderId)
+    order = list(order.values())
+    return JsonResponse({'order': order})
+
+def addOrder(request):
+    if request.POST:
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            if form.save():
+                return JsonResponse('Order created')
+            else:
+            	return JsonResponse('There was an error')
+        else:
+            return JsonResponse('Invalid form details')
+    else:
+        form = OrderForm()
+        return render(request, 'add.html', {'form':form})
+
+def editOrder(request, orderId):
+    order = Order.objects.get(id=orderId)
+    if request.POST:
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            if form.save():
+                return JsonResponse('Order updated')
+            else:
+                return JsonResponse('There was an error')
+        else:
+            return JsonResponse('Invalid form details')
+    else:
+        form = OrderForm(instance=order)
+        return render(request, 'edit.html', {'form':form})
+
+def deleteOrder(request, orderId):
+    order = Order.objects.get(id=orderId)
+    order.delete()
+    return JsonResponse('Order deleted')
